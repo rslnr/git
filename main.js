9d875ac62f29b05c84d100c1a78c831b9f7985ef
rslnr <rakhmanov915@gmail.com>
2026-02-24 14:05:10 +0400

    Remove login functionality from main.js
    Removed login function and related alert messages.

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
-function login () {
-    const user = document.getElementById("username").value;
-    const pass = document.getElementById("password").value;
-    if (user === "admin" && pass === "admin") {
-        alert ("successful login!")
+'use strict';
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.check').addEventListener('click', function() {
+    const guess = Number(document.querySelector('.guess').value);
+    if (!guess) {
+        document.querySelector('.message').textContent = 'No Number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = 'Correct Number!';
+    } else if (guess < secretNumber) {
+        document.querySelector('message').textContent = 'Too Low!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = 'Too High!';
     }
-    else {
-        alert ("incorrect login or password, please try again!")
-    }
-}
-login()
+})
